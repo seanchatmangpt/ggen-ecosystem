0913fc245bff279a193df401825886f5562ffe61
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-29 13:42:09 -0700

    feat(chicago): activate zero-or-one-path consumer

---
+++ tools/chicago_scenarios/scenario_096_zero_or_one_path.py
@@ -0,0 +1,5 @@
+from chicago_consumer_matrix import Scenario
+
+SCENARIO = Scenario("zero-or-one-path", '''PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+SELECT ?value WHERE { ?s rdf:type? ?value } ORDER BY ?value''', 5)
+
